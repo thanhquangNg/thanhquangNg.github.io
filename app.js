3efea6b82ed0d7fd3f8531489213696562305895
Quang Nguyen <thanhquang0912@gmail.com>
2022-07-04 09:42:52 +0200

    update progress

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const section = document.querySelectorAll('.section');
+const sections = document.querySelectorAll('.section');
 const sectBtns = document.querySelectorAll('.controlls');
 const sectBtn = document.querySelectorAll('.control');
 const allSections = document.querySelector('.main-content');
@@ -13,6 +13,27 @@ function PageTransitions(){
             sectBtn[i].className += ' active-btn';
         })
     }
+
+    //Sections active class
+    allSections.addEventListener('click',(e) =>{
+        const id = e.target.dataset.id;
+        //console.log(id)
+        if(id){
+            //remove selected from the other btns
+            sectBtns.forEach((btn)=>{
+                btn.classList.remove('active')
+            })
+            e.target.classList.add('active');
+
+            // hide other sections
+            sections.forEach((section)=>{
+                section.classList.remove('active');
+            })
+
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }
+    })
 }
 
 PageTransitions();
